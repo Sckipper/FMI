dc7ba376d4948adba3687bf72da5941dce631d2d
Sckipper <yo_da_daniel@yahoo.com>
2017-03-12 02:31:58 +0200

    Started project

---
+++ Licenta/Licenta/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Licenta.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Authentication()
+        {
+            return View();
+        }
+    }
+}
---
+++ Licenta/Licenta/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="3rdparty/bootstrap.min.js" />
+/// <reference path="3rdparty/jquery.validate.min.js" />
+/// <reference path="3rdparty/jquery.validate.unobtrusive.min.js" />
+/// <reference path="3rdparty/jquery-1.10.2.min.js" />
+/// <reference path="3rdparty/modernizr-2.6.2.js" />
+/// <reference path="3rdparty/respond.js" />
